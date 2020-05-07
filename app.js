e061f0a20ad02cbff27f0ba3b71258261d3c565c
imac.yohan <yohan.kim@innoboost.co.kr>
2020-05-07 18:17:51 +0900

    불필요코드 제거, route에 status코드 삽입

--- app.js
+++ app.js
@@ -36,11 +36,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
-app.use *
-  // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    next(createError(404));
-  });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 router.post('/login', (req, res, next) => {
-    console.log('login');
-    res.json("You logined Successfully!");
+    console.log('login', req.body);
+
+    res.status(200).json("sucess");
+
 });
 module.exports = router;
