f701ab70339375f4adff9bb7d7521eb40d57da28
Yohan <yohan@yohanpro.com>
2020-05-10 19:44:27 +0900

    1. 로그인시 간단 정보 돌려주기(그냥 돌려주는 용도):

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,17 @@
 var express = require("express");
 var router = express.Router();
+const jwt = require("jwt-simple");
 
-router.post('/login', (req, res, next) => {
-    console.log('login', req.body);
+router.post("/login", (req, res, next) => {
+  //지금이야 딱히 상관없음 예시일뿐
+  let data = req.body;
+  console.log("data", data);
+  const user = {
+    email: data.email,
+    name: data.name,
+  };
+  console.log("user", user);
 
-    res.status(200).json("sucess");
-
+  res.status(200).json(user);
 });
 module.exports = router;
